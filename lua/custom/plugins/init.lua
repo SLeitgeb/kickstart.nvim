e32913ff6394b0e72b39c80e0e7e96ea76e09542
Šimon Leitgeb <leitgeb.simon@gmail.com>
2024-08-14 11:07:42 +0200

    Enable automatic light/dark mode switching

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -47,4 +47,11 @@ return {
   },
   'theHamsta/nvim-dap-virtual-text',
   'farmergreg/vim-lastplace',
+  {
+    '4e554c4c/darkman.nvim',
+    build = 'go build -o bin/darkman.nvim',
+    config = function()
+      require 'darkman'.setup()
+    end,
+  }
 }
